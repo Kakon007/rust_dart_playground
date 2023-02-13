18c25315a2ceb3a4b3525d39a07c4122abd81244
kakon007 <kakon443@gmail.com>
2023-02-13 11:38:05 +0600

    watch a file dir in rust

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,42 @@
-use std::fs;
+extern crate notify;
+
+use std::time::Duration;
 use std::path::Path;
-
-fn main() {
-    let folder_path = Path::new("/home/jahid007/rust_play_folder/Jahid");
-    let file_path = Path::new("/home/jahid007/rust_play_folder/Jahid/file.txt");
+use notify::{RecommendedWatcher, RecursiveMode, Result, recommended_watcher, Watcher};
+use std::thread;
 
 
-    match fs::create_dir(folder_path) {
-        Ok(_) => println!("Folder created"),
-        Err(err) => println!("Failed to create folder: {}", err),
-    }
 
-    match fs::write(file_path, "Hello, world!") {
-        Ok(_) => println!("File created"),
-        Err(err) => println!("Failed to create file: {}", err),
-    }
+
+// fn main() {
+//     let folder_path = Path::new("/home/jahid007/rust_play_folder/Jahid");
+//     let file_path = Path::new("/home/jahid007/rust_play_folder/Jahid/file.txt");
+
+
+//     match fs::create_dir(folder_path) {
+//         Ok(_) => println!("Folder created"),
+//         Err(err) => println!("Failed to create folder: {}", err),
+//     }
+
+//     match fs::write(file_path, "Hello, world!") {
+//         Ok(_) => println!("File created"),
+//         Err(err) => println!("Failed to create file: {}", err),
+//     }
+// }
+
+fn main() -> Result<()> {
+    // Automatically select the best implementation for your platform.
+    let mut watcher: RecommendedWatcher = recommended_watcher(|res| {
+        match res {
+           Ok(event) => println!("event: {:?}", event),
+           Err(e) => println!("watch error: {:?}", e),
+        }
+    })?;
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(Path::new("/home/jahid007/rust_play_folder/Jahid"), RecursiveMode::Recursive)?;
+
+    thread::sleep(Duration::from_secs(10));
+    Ok(())
 }
