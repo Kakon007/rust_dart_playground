7c34b276459896c8b0895bffdc11d4812e100268
kakon007 <kakon443@gmail.com>
2023-02-14 10:12:01 +0600

    dir copy

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,9 @@
 // // use notify::{RecommendedWatcher, RecursiveMode, Result, recommended_watcher, Watcher};
 // // use std::thread;
 
-
-
-
 // fn main() {
 //     let folder_path = Path::new("/home/jahid007/rust_play_folder/Jahid");
 //     let file_path = Path::new("/home/jahid007/rust_play_folder/Jahid/file.txt");
-
 
 //     match fs::create_dir(folder_path) {
 //         Ok(_) => println!("Folder created"),
@@ -37,26 +33,46 @@
 //     Ok(())
 // }
 
-use std::io;
-use rand::Rng;
+// use std::io;
+// use rand::Rng;
+
+// fn main() {
+//     println!("Guess the number!");
+
+//     println!("Please input your guess.");
+
+//     let mut guess = String::new();
+//     io::stdin()
+//     .read_line(&mut guess)
+//     .expect("Failed to read line");
+
+//     let secret_number = rand::thread_rng().gen_range(1..=100);
+
+//     println!("The secret number is: {secret_number}");
+
+//     println!("You guessed: {guess}");
+// }
+
+use fs_extra::dir::CopyOptions;
+
+// fn main(){
+//   let from= "/home/jahid007/rust_play_folder/Jahid";
+//     let to = "/home/jahid007/rust_play_folder/Jahid2";
+//     let options = CopyOptions::new();
+//     fs_extra::dir::copy(from, to, &options).unwrap();
+// }
+
+use std::path::Path;
 
 fn main() {
-    println!("Guess the number!");
-
-    
-
-    println!("Please input your guess.");
-
-    let mut guess = String::new();
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("The secret number is: {secret_number}");
-
-   
-
-    println!("You guessed: {guess}");
+    let path = "/home/jahid007/rust_play_folder/Jahid";
+    let from = "/home/jahid007/rust_play_folder/Jahid";
+    let to = "/home/jahid007/rust_play_folder/jahid2";
+    let options = CopyOptions::new();
+    if Path::new(path).exists() && Path::new(path).is_dir() {
+        println!("The folder exists.");
+        fs_extra::dir::copy(from, to, &options).unwrap();
+    } else {
+        println!("The folder does not exist.");
+    }
 }
